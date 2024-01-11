9d0e1c8903c1ab2bdbbc675c28b3bdc676113c5f
min486 <smh92333@gmail.com>
2024-01-11 18:15:36 +0900

    feat: Splash Screen 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -69,6 +69,9 @@ dependencies {
     // viewmodel-compose
     // implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
 
+    // splash
+    implementation("androidx.core:core-splashscreen:1.0.1")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/desktop/k_city/MainActivity.kt
+++ app/src/main/java/desktop/k_city/MainActivity.kt
@@ -3,13 +3,25 @@ package desktop.k_city
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.runtime.Composable
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import desktop.k_city.ui.screens.KCityNavGraph
 import desktop.k_city.ui.theme.KcityTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel by viewModels<MainViewModel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Thread.sleep(1000)
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                !viewModel.isReady.value
+            }
+        }
+
         setContent {
             KcityTheme {
                 kCityApp()
